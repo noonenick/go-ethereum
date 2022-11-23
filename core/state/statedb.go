2efc5985f7017ab1c6c447dd3c9b8522a3cf96cc
code <code@example.com>
2022-11-23 23:56:29 +0800

    fixbug

--- core/state/statedb.go
+++ core/state/statedb.go
@@ -1501,3 +1501,8 @@ func (s *StateDB) Witness() *stateless.Witness {
 func (s *StateDB) AccessEvents() *AccessEvents {
 	return s.accessEvents
 }
+
+// GetAccessList 
+func (s *StateDB) GetAccessList() map[string]interface{} {
+	return s.accessList.GetAccessList()
+}
