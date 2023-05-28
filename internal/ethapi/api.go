4eb0693591d18e85779066c6e961a6129240eec4
code <code@example.com>
2023-05-28 21:01:09 +0800

    support calls bundle, set return default

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -2632,7 +2632,11 @@ func (s *BundleAPI) SearchBundle(ctx context.Context, args SearchBundleArgs) (ma
 
 	prevLen := len(txs)
 
-	callMask := new(CallMaskArgs)
+	//callMask := new(CallMaskArgs)
+	markTrue := true
+	callMask := &CallMaskArgs{
+		Return: &markTrue,
+	}
 
 	for i, txArgs := range args.Calls {
 		if args.InitBalance != nil {
