8d0368019fafcfc94d11de72824f8a106c3592d8
code <code@example.com>
2023-08-30 15:43:12 +0800

    fix merge

--- core/state_processor.go
+++ core/state_processor.go
@@ -387,7 +387,7 @@ func onSystemCallStart(tracer *tracing.Hooks, ctx *tracing.VMContext) {
 }
 func ApplyTransactionWithResult(config *params.ChainConfig, bc ChainContext, author *common.Address, gp *GasPool, statedb *state.StateDB, header *types.Header, tx *types.Transaction, usedGas *uint64, cfg vm.Config) (*types.Receipt, *ExecutionResult, error) {
 	//msg, err := tx.AsMessage(types.MakeSigner(config, header.Number), header.BaseFee)
-	msg, err := TransactionToMessage(tx, types.MakeSigner(config, header.Number), header.BaseFee)
+	msg, err := TransactionToMessage(tx, types.MakeSigner(config, header.Number, header.Time), header.BaseFee)
 	if err != nil {
 		return nil, nil, err
 	}
