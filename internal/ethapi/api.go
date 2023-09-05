f2d7a1ec8939923b19fef2a99b2a8b0b2042915c
code <code@example.com>
2023-09-05 10:48:29 +0800

    add gasUsed when err

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -2761,6 +2761,7 @@ func (s *BundleAPI) SearchBundle(ctx context.Context, args SearchBundleArgs) (ma
 		}
 
 		jsonResult := map[string]interface{}{}
+		jsonResult["gasUsed"] = result.UsedGas
 		if result.Err != nil {
 			jsonResult["error"] = result.Err.Error()
 			revert := result.Revert()
@@ -2772,7 +2773,6 @@ func (s *BundleAPI) SearchBundle(ctx context.Context, args SearchBundleArgs) (ma
 				break
 			}
 		} else {
-			jsonResult["gasUsed"] = result.UsedGas
 			if callMask.Return != nil && *callMask.Return {
 				dst := make([]byte, hex.EncodedLen(len(result.Return())))
 				hex.Encode(dst, result.Return())
